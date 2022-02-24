2256fab6b4e046b584159fd19aa659c6c5444532
C2H8N2 <y.dusoleil@outlook.com>
2022-02-24 15:43:49 +0100

    Suppression pop-up

--- javascript/index.js
+++ javascript/index.js
@@ -8,8 +8,3 @@ function pause_play(){
     document.getElementById("play_pause").value = "Pause Video"
   }
 }
-
-function enlever_le_message(){
-  document.getElementById("pop_up_bonne_note_2021_bg").style.display = "none";
-  document.getElementById("pop_up_bonne_note_2021").style.display = "none"; 
-}
